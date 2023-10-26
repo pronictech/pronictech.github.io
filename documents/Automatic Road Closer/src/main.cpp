9b86c5eeb0593096a90d4fc2417fdc27ddc753e8
Pronic-Tech Association <136002585+pronictech@users.noreply.github.com>
2023-10-26 22:38:49 +0100

    Add files via upload
    thanks

---
+++ documents/Automatic Road Closer/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#define led 2
+
+
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led,HIGH);
+  delay(2000);
+digitalWrite(led,LOW);
+  delay(2000);
+}
